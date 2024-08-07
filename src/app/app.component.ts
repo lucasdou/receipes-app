de8b26aec6382435e53542869b47d674e27c2d33
lucas <lucas.douillard@hotmail.fr>
2024-08-07 23:13:17 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListItemComponent } from './list-item/list-item.component';
+import { DataService } from './service/data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'receipes-app';
+  public myDataService : any;
+
+constructor(private dataService: DataService) {}
+
+ngOnInit(): void {
+  this.myDataService = this.dataService.getObject();
 }
+}
+
+
---
+++ src/app/service/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  public receipes = [
+    {name: 'receipe1', description: 'description1', ingredients: 'ingredients1', instructions: 'instructions1'},
+    {name: 'receipe2', description: 'description2', ingredients: 'ingredients2', instructions: 'instructions2'},
+    {name: 'receipe3', description: 'description3', ingredients: 'ingredients3', instructions: 'instructions3'}
+];
+
+  getObject() {
+    return this.receipes;
+  }
+
+  setObject(newObject: any) {
+    this.receipes = newObject;
+  }
+}
