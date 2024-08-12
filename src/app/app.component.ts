0b69ced0bab40d4b19c730709c9b3f2b7bcf1fde
lucas <lucas.douillard@hotmail.fr>
2024-08-12 21:27:45 +0200

    feature/FRTL-10-create-a-taskbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { ListItemComponent } from './component/list-item/list-item.component';
 import { DataService } from './service/data.service';
 import { MyListComponent } from './component/my-list/my-list.component';
 import { Receipe } from './object/Receipe';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListItemComponent, MyListComponent],
+  imports: [RouterOutlet, ListItemComponent, MyListComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
